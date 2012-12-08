962475f3c0ab9afb8b5cf0cc91d733da8519ae76
Georg Eitler <funer@gmx.at>
2012-12-08 21:54:21 +0100

    make the frame wider in the config, to run more nicely with the layout

--- config.lua
+++ config.lua
@@ -9,7 +9,7 @@ ns.cfdGather = false				-- [Plugin] log Current Fight Data? [true/false]
 ns.sync = false						-- [Plugin] sync data from other Wham users? [true/false]
 ns.solo_hide = false				-- gathers no data, until in a group or raid [true/false]
 ns.autoreset = false
-ns.width = 350
+ns.width = 400
 ns.height = 170
 
 -- tempPets contains all temporary summoned pets, which will be added to the summoners/owners damage
