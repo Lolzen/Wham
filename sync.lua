d74aaad550be5c2cd665d73bb862d802f06a3dd3
Georg Eitler <funer@gmx.at>
2012-12-14 00:13:28 +0100

    update code

--- sync.lua
+++ sync.lua
@@ -8,6 +8,7 @@ local addon, ns = ...
 ns.syncFrame = CreateFrame("Frame", "syncFrame", UIParent)
 ns.syncFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
 ns.syncFrame:RegisterEvent("CHAT_MSG_ADDON")
+ns.syncFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 
 function ns.syncFrame:PLAYER_ENTERING_WORLD()
 	RegisterAddonMessagePrefix("Wham_DMG")
@@ -15,74 +16,101 @@ function ns.syncFrame:PLAYER_ENTERING_WORLD()
 	RegisterAddonMessagePrefix("Wham_ABSORB")
 end
 
+local localDmg, localHeal, localAbsorb
 function ns.syncFrame:CHAT_MSG_ADDON(self, arg1, arg2, arg3, arg4)
 	local prefix, msg, channel, sender = arg1, arg2, arg3, arg4
+	--print(channel..": "..prefix.." - From: ["..sender.."]")
 	-- Everything Damage related
 	if prefix == "Wham_DMG" then
 		-- Gathering Messages sent and converting them so we can work with them
-		extDmgName, extDmg_raw = strsplit(" ", msg, 2)
+		local extDmgName_raw, extDmg_raw = strsplit(" ", msg, 2)
 		-- We can't compare strings to numbers, so we have to convert that
-		extDmg = tonumber(extDmg_raw, A)
+		local extDmg = tonumber(extDmg_raw, A)
+		-- Attach realm to the Name, so we don't get duplicates
+		local name, realm = UnitName(extDmgName_raw)
+		realm = realm and realm ~= "" and "-"..realm or ""
+		local extDmgName = extDmgName_raw..realm
 		
-		-- k = name
 		-- v = dmg
-		for k, v in pairs(ns.dmgData) do
-			localDmgName = k
+		for extDmgName, v in pairs(ns.dmgData) do
 			localDmg = v
 		end
-		
-		-- Check if the names match, so we don't override somone else's data
-		if extDmgName == localDmgName then
-			-- If the external DmgData is more up to date, sync it
-			if extDmg > localDmg then
+
+		-- initialize if nil
+		if localDmg == nil then
+			localDmg = 0
+		end
+
+		ns.watched[extDmgName] = true
+		if extDmg > localDmg then
+			if ns.watched[extDmgName] then
 				ns.dmgData[extDmgName] = extDmg
+				ns.wham:UpdateLayout()
 			end
 		end
 	end
 	-- Everything Heal related
 	if prefix == "Wham_HEAL" then
 		-- Gathering Messages sent and converting them so we can work with them
-		extHealName, extHeal_raw = strsplit(" ", msg, 2)
+		local extHealName_raw, extHeal_raw = strsplit(" ", msg, 2)
 		-- We can't compare strings to numbers, so we have to convert that
-		extHeal = tonumber(extHeal_raw, A)
-		
-		-- k = name
+		local extHeal = tonumber(extHeal_raw, A)
+		-- Attach realm to the Name, so we don't get duplicates
+		local name, realm = UnitName(extHealName_raw)
+		realm = realm and realm ~= "" and "-"..realm or ""
+		local extHealName = extHealName_raw..realm
+
 		-- v = heal
-		for k, v in pairs(ns.healData) do
-			localHealName = k
+		for extHealName, v in pairs(ns.healData) do
 			localHeal = v
 		end
-		
-		-- Check if the names match, so we don't override somone else's data
-		if extHealName == localHealName then
-			-- If the external HealData is more up to date, sync it
-			if extHeal > localHeal then
+
+		-- initialize if nil
+		if localHeal == nil then
+			localHeal = 0
+		end
+
+		ns.watched[extHealName] = true
+		if extHeal > localHeal then
+			if ns.watched[extHealName] then
 				ns.healData[extHealName] = extHeal
+				ns.wham:UpdateLayout()
 			end
 		end
-	end
+--	end
 	-- Everything Absorb related
 	if prefix == "Wham_ABSORB" then
 		-- Gathering Messages sent and converting them so we can work with them
-		extAbsorbName, extAbsorb_raw = strsplit(" ", msg, 2)
+		local extAbsorbName_raw, extAbsorb_raw = strsplit(" ", msg, 2)
 		-- We can't compare strings to numbers, so we have to convert that
-		extAbsorb = tonumber(extAbsorb_raw, A)
-		
-		-- k = name
+		local extAbsorb = tonumber(extAbsorb_raw, A)
+		-- Attach realm to the Name, so we don't get duplicates
+		local name, realm = UnitName(extAbsorbName_raw)
+		realm = realm and realm ~= "" and "-"..realm or ""
+		local extAbsorbName = extAbsorbName_raw..realm
+
 		-- v = absorb
-		for k, v in pairs(ns.absorbData) do
-			localAbsorbName = k
+		for extAbsorbName, v in pairs(ns.absorbData) do
 			localAbsorb = v
 		end
-		
-		-- Check if the names match, so we don't override somone else's data
-		if extAbsorbName == localAbsorbName then
-			-- If the external AbsorbData is more up to date, sync it
-			if extAbsorb > localAbsorb then
-				ns.absorbdData[extAbsorbName] = extAbsorb
+
+		-- initialize if nil
+		if localAbsorb == nil then
+			localAbsorb = 0
+		end
+
+		ns.watched[extAbsorbName] = true		
+		if extAbsorb > localAbsorb then
+			if ns.watched[extAbsorbName] then
+				ns.absorbData[extAbsorbName] = extAbsorb
+				ns.wham:UpdateLayout()
 			end
 		end
 	end
+end
+
+function ns.syncFrame:COMBAT_LOG_EVENT_UNFILTERED()
+	ns.wham:UpdateLayout()
 end
 
 ns.syncFrame:SetScript("OnEvent", function(self, event, ...)  
